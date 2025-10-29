d9a6f52632fdb2cdfcaa81745371f4e2b093b350
Olamide <pemzyj123@gmail.com>
2025-10-29 21:05:55 +0100

    edited .env file for database hosting

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
 async function startServer() {
   try {
     await initDatabase();
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0/0', () => {
       console.log(`Server running on port ${PORT}`);
       console.log(`http://localhost:${PORT}`);
     });
